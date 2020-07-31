0781d0ebae9b28b141e5fd5320a1e32df72a607b
Jochen Linnemann <jl@jochenlinnemann.de>
2020-07-31 11:16:30 +0200

    Switch from x86 to x64

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ SET ORIG=%CD%
 REM SET GOPATH=%CD%\src
 SET GOBIN=%CD%\bin
 REM Support for older architectures
-SET GOARCH=386
+SET GOARCH=amd64
 
 REM Cleanup existing build if it exists
 if exist src\nvm.exe (
@@ -48,7 +48,7 @@ REM Generate checksums
 for %%f in (%DIST%\*.*) do (certutil -hashfile "%%f" MD5 | find /i /v "md5" | find /i /v "certutil" >> "%%f.checksum.txt")
 
 REM Cleanup
-del %GOBIN%\nvm.exe
+REM del %GOBIN%\nvm.exe
 
 echo NVM for Windows v%AppVersion% build completed.
 @echo on
